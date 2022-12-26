73250cb6458813c19cf961f6c12f7f6221ea2f0b
Noah Kim <dongwoo_333@hanmail.net>
2022-12-26 17:38:54 +1100

    feat: add animation and an orbit

--- 02-initial-tutorial/src/js/script.js
+++ 02-initial-tutorial/src/js/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const renderer = new THREE.WebGLRenderer()
 
@@ -10,9 +11,13 @@ const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
+// orbit
+const orbit = new OrbitControls(camera, renderer.domElement)
+
 // camera.position.z = 5
 // camera.position.y = 2
 camera.position.set(0, 2, 5)
+orbit.update() // everytime camera position changes
 
 // add Helper Axes
 const axesHelper = new THREE.AxesHelper(5)
@@ -24,4 +29,10 @@ const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
 const box = new THREE.Mesh(boxGeometry, boxMaterial)
 scene.add(box)
 
-renderer.render(scene, camera)
+function animate(time) {
+  box.rotation.x = time / 1000
+  box.rotation.y = time / 1000
+  renderer.render(scene, camera)
+}
+
+renderer.setAnimationLoop(animate)
