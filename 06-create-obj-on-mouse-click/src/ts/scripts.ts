7a3f245c5d81bdacb4501aaf70036e8bec3b1072
Noah Kim <dongwoo_333@hanmail.net>
2023-01-04 21:11:18 +1100

    6. Create Objects On Mouse Click: init

---
+++ 06-create-obj-on-mouse-click/src/ts/scripts.ts
@@ -0,0 +1,39 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+camera.position.set(0, 5, 5);
+orbit.update();
+
+const ambientLight = new THREE.AmbientLight(0x33333);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+scene.add(directionalLight);
+directionalLight.position.set(0, 50, 0);
+
+const axesHelper = new THREE.AxesHelper(20);
+scene.add(axesHelper);
+
+function animate() {
+  renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
+window.addEventListener('resize', function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
