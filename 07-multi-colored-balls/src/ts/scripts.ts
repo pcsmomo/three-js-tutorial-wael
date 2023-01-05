d2cd13fad8a3697945994d4cad552fad392038ad
Noah Kim <dongwoo_333@hanmail.net>
2023-01-05 17:21:54 +1100

    7. Create Multi-Colored Bouncing Balls: init (based on 06)

---
+++ 07-multi-colored-balls/src/ts/scripts.ts
@@ -0,0 +1,66 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+camera.position.set(0, 5, 5);
+orbit.update();
+
+const ambientLight = new THREE.AmbientLight(0x33333);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+scene.add(directionalLight);
+directionalLight.position.set(0, 50, 0);
+
+// const axesHelper = new THREE.AxesHelper(20);
+// scene.add(axesHelper);
+
+const mouse = new THREE.Vector2();
+const intersectionPoint = new THREE.Vector3();
+const planeNormal = new THREE.Vector3();
+const plane = new THREE.Plane();
+const raycaster = new THREE.Raycaster();
+
+window.addEventListener('mousemove', function (e) {
+  mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
+  planeNormal.copy(camera.position).normalize();
+  plane.setFromNormalAndCoplanarPoint(planeNormal, scene.position);
+  raycaster.setFromCamera(mouse, camera);
+  raycaster.ray.intersectPlane(plane, intersectionPoint);
+});
+
+window.addEventListener('click', function () {
+  const sphereGeo = new THREE.SphereGeometry(0.125, 30, 30);
+  const sphereMat = new THREE.MeshStandardMaterial({
+    color: 0xffea00,
+    metalness: 0,
+    roughness: 0
+  });
+  const sphereMesh = new THREE.Mesh(sphereGeo, sphereMat);
+  scene.add(sphereMesh);
+  sphereMesh.position.copy(intersectionPoint);
+});
+
+function animate() {
+  renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
+window.addEventListener('resize', function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
