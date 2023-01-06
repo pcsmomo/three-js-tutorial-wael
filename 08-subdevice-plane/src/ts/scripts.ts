e911f28a98dae7e5aed6a4c82999ca0a0cb82a9b
Noah Kim <dongwoo_333@hanmail.net>
2023-01-06 14:46:14 +1100

    8. How To Subdivide A Plane Into Even Squares: init

---
+++ 08-subdevice-plane/src/ts/scripts.ts
@@ -0,0 +1,29 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+camera.position.set(10, 15, -22);
+orbit.update();
+
+function animate() {
+  renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
+window.addEventListener('resize', function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
