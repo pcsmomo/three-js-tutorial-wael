7a51248ffa1f31acf36b4d217e3cc892a37124eb
Noah Kim <dongwoo_333@hanmail.net>
2023-01-11 19:26:40 +1100

    feat: load hrd image

--- 11-realistic-scenes-tone-mapping/src/ts/scripts.ts
+++ 11-realistic-scenes-tone-mapping/src/ts/scripts.ts
@@ -1,5 +1,17 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
+
+const hrdImages = [
+  'MR_INT-001_NaturalStudio_NAD.hdr',
+  'MR_INT-002_BathroomHard_Pierre.hdr',
+  'MR_INT-003_Kitchen_Pierre.hdr',
+  'MR_INT-004_BigWindowTree_Thea.hdr',
+  'MR_INT-005_WhiteNeons_NAD.hdr',
+  'MR_INT-006_LoftIndustrialWindow_Griffintown.hdr'
+];
+
+const hrdTextureUrl = new URL(`../img/${hrdImages[2]}`, import.meta.url);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 
@@ -15,6 +27,12 @@ const orbit = new OrbitControls(camera, renderer.domElement);
 
 camera.position.set(0, 0, 7);
 orbit.update();
+
+const loader = new RGBELoader();
+loader.load(hrdTextureUrl.href, function (texture) {
+  texture.mapping = THREE.EquirectangularReflectionMapping;
+  scene.background = texture;
+});
 
 function animate() {
   renderer.render(scene, camera);
