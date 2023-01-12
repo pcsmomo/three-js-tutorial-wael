b54f0c8e4e4c3631b52d4a73789f93ee18e7f5e9
Noah Kim <dongwoo_333@hanmail.net>
2023-01-12 18:16:59 +1100

    feat: copy the parcel boilerplate

---
+++ 12-import-model-skechfab/src/ts/scripts.ts
@@ -0,0 +1,35 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+camera.position.set(0, 0, 5);
+orbit.update();
+
+// add Box
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box);
+
+function animate() {
+  renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
+window.addEventListener('resize', function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
