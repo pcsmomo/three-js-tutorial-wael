1a4592aa3bbf1d80bd4d1d467a1d1b4a80f298ad
Noah Kim <dongwoo_333@hanmail.net>
2023-01-13 15:38:22 +1100

    12. How To Import A Model From Sketchfab.com: init

--- 12-import-model-skechfab/src/ts/scripts.ts
+++ 12-import-model-skechfab/src/ts/scripts.ts
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
 
 const renderer = new THREE.WebGLRenderer();
 
@@ -11,16 +13,28 @@ const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+renderer.setClearColor(0xa3a3a3);
+
 const orbit = new OrbitControls(camera, renderer.domElement);
 
-camera.position.set(0, 0, 5);
+camera.position.set(6, 6, 6);
 orbit.update();
 
-// add Box
-const boxGeometry = new THREE.BoxGeometry();
-const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const box = new THREE.Mesh(boxGeometry, boxMaterial);
-scene.add(box);
+// add helper
+const grid = new THREE.GridHelper(30, 30);
+scene.add(grid);
+
+const ambientLight = new THREE.AmbientLight(0xededed, 0.8);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+scene.add(directionalLight);
+directionalLight.position.set(10, 11, 7);
+
+// loaders
+const gltfLoader = new GLTFLoader();
+
+const rgbeLoader = new RGBELoader();
 
 function animate() {
   renderer.render(scene, camera);
---
+++ 12-import-model-skechfab/static/gltf-file-download.md
@@ -0,0 +1,3 @@
+# [perosnal google drive folder](https://drive.google.com/drive/folders/1sxUz-kgRMhy4ypCBA3EZloIseSPKgTB0?usp=sharing)
+
+gltf -> free_1975_porsche_911_930_turbo.zip
