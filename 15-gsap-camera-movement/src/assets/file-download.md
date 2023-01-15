f6c75247bd46806d929ed3d74a125ee1b3a1406e
Noah Kim <dongwoo_333@hanmail.net>
2023-01-15 19:39:13 +1100

    15. Camera Movement Using GSAP: init

---
+++ 15-gsap-camera-movement/src/assets/file-download.md
@@ -0,0 +1,3 @@
+# [perosnal google drive folder](https://drive.google.com/drive/folders/1sxUz-kgRMhy4ypCBA3EZloIseSPKgTB0?usp=sharing)
+
+- gltf -> `phoenix_bird.zip`
---
+++ 15-gsap-camera-movement/src/ts/scripts.ts
@@ -0,0 +1,50 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+renderer.setClearColor(0xa3a3a3);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+orbit.update();
+
+camera.position.set(0, 2, 5);
+camera.lookAt(0, 0, 0);
+
+// add light
+const ambientLight = new THREE.AmbientLight(0xededed, 0.8);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+scene.add(directionalLight);
+directionalLight.position.set(0, 20, -20);
+
+const grid = new THREE.GridHelper(30, 30);
+scene.add(grid);
+
+// add Box
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box);
+box.position.y = 0.5;
+
+function animate() {
+  renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
+window.addEventListener('resize', function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
