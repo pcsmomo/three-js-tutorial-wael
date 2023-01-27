bda599487332510f515decc0bb2dfb83e3124afb
Noah Kim <dongwoo_333@hanmail.net>
2023-01-27 17:21:00 +1100

    feat: add dat.gui controller

--- 16-gsap-kings-hall-dat.gui/src/ts/scripts.ts
+++ 16-gsap-kings-hall-dat.gui/src/ts/scripts.ts
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import gsap from 'gsap';
+import * as dat from 'dat.gui';
 
 const renderer = new THREE.WebGLRenderer();
 
@@ -16,6 +16,25 @@ renderer.setClearColor(0xa3a3a3);
 
 camera.position.set(-1.7, 0, 8.7);
 camera.lookAt(1.7, 0, 8.7);
+
+// GUI
+const gui = new dat.GUI();
+
+const options = {
+  'position X': -1.7,
+  'position Y': 0,
+  'position Z': 8.7,
+  'rotation X': 0,
+  'rotation Y': -1.57,
+  'rotation Z': 0
+};
+
+gui.add(options, 'position X', -5, 5);
+gui.add(options, 'position Y', -2, 3);
+gui.add(options, 'position Z', -2, 16);
+gui.add(options, 'rotation X', -10, 10);
+gui.add(options, 'rotation Y', -4.7, 1.7);
+gui.add(options, 'rotation Z', -3.5, 3.5);
 
 // loaders
 const loadingManager = new THREE.LoadingManager();
@@ -40,75 +59,30 @@ const gltfLoader = new GLTFLoader(loadingManager);
 renderer.outputEncoding = THREE.sRGBEncoding;
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
 
-let position = 0;
-
 // load model
 gltfLoader.load('./assets/kings-hall/scene.gltf', function (gltf) {
   const model = gltf.scene;
   scene.add(model);
+});
 
-  window.addEventListener('mouseup', function () {
-    switch (position) {
-      case 0:
-        moveCamera(-1.8, 1.6, 5);
-        rotateCamera(0, 0.1, 0);
-        position = 1;
-        break;
-      case 1:
-        moveCamera(2.8, 0, 3.6);
-        rotateCamera(0, -2, 0);
-        position = 2;
-        break;
-      case 2:
-        moveCamera(2.5, -0.9, 12.2);
-        rotateCamera(0.9, 0.6, -0.6);
-        position = 3;
-        break;
-      case 3:
-        moveCamera(-2.7, 0.6, 3.7);
-        rotateCamera(0.6, 1.9, -0.6);
-        position = 4;
-        break;
-      case 4:
-        moveCamera(-1.7, 0, 8.7);
-        rotateCamera(0, 4.7, 0);
-        position = 5;
-        break;
-      case 5:
-        moveCamera(0.5, 0.8, 10);
-        rotateCamera(0.3, 1.65, -0.3);
-        position = 0;
-        break;
-    }
-  });
-
-  window.addEventListener('keypress', function () {
-    const { x: pX, y: pY, z: pZ } = camera.position;
-    const { x: rX, y: rY, z: rZ } = camera.rotation;
-    console.info(`pos: (${pX}, ${pY}, ${pZ}), rotate: (${rX}, ${rY}, ${rZ})`);
-  });
-
-  function moveCamera(x: number, y: number, z: number) {
-    gsap.to(camera.position, {
-      x,
-      y,
-      z,
-      duration: 3
-    });
-  }
-
-  function rotateCamera(x: number, y: number, z: number) {
-    gsap.to(camera.rotation, {
-      x,
-      y,
-      z,
-      duration: 3.2
-    });
-  }
+window.addEventListener('keypress', function () {
+  const { x: pX, y: pY, z: pZ } = camera.position;
+  const { x: rX, y: rY, z: rZ } = camera.rotation;
+  console.info(`pos: (${pX}, ${pY}, ${pZ}), rotate: (${rX}, ${rY}, ${rZ})`);
 });
 
 function animate() {
   renderer.render(scene, camera);
+
+  const pX = options['position X'];
+  const pY = options['position Y'];
+  const pZ = options['position Z'];
+  const rX = options['rotation X'];
+  const rY = options['rotation Y'];
+  const rZ = options['rotation Z'];
+
+  camera.position.set(pX, pY, pZ);
+  camera.rotation.set(rX, rY, rZ);
 }
 
 renderer.setAnimationLoop(animate);
