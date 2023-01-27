d47938475f5b65b117515b4b24bb7f9ceec386f7
Noah Kim <dongwoo_333@hanmail.net>
2023-01-27 18:03:28 +1100

    17. Path Following Steering Behavior With Yuka - AI Tutorial: init

---
+++ 17-yuka-path-following/src/ts/scripts.ts
@@ -0,0 +1,26 @@
+import * as THREE from 'three';
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+camera.position.set(0, 0, 5);
+camera.lookAt(scene.position);
+
+function animate() {
+  renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
+window.addEventListener('resize', function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
