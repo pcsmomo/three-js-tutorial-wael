9cad6d4b145496bcd3fa98aad9f65f835c62e347
Noah Kim <dongwoo_333@hanmail.net>
2023-01-27 18:55:51 +1100

    feat: extract waypoints and add them to line mesh

--- 17-yuka-path-following/src/ts/scripts.ts
+++ 17-yuka-path-following/src/ts/scripts.ts
@@ -30,8 +30,15 @@ function sync(entity: YUKA.GameEntity, renderComponent: any) {
   renderComponent.matrix.copy(entity.worldMatrix);
 }
 
+// @types/yuka version is outdated
+type Path = YUKA.Path & {
+  _waypoints: YUKA.Vector3[];
+  _index: number;
+};
+
 // Path
-const path = new YUKA.Path();
+// @ts-ignore
+const path: Path = new YUKA.Path();
 path.add(new YUKA.Vector3(-4, 0, 4));
 path.add(new YUKA.Vector3(-6, 0, 0));
 path.add(new YUKA.Vector3(-4, 0, -4));
@@ -49,6 +56,19 @@ vehicle.steering.add(followPathBehavior);
 // Entity Manager
 const entityManager = new YUKA.EntityManager();
 entityManager.add(vehicle);
+
+const position = []; // (x, y, z)[]
+for (let i = 0; i < path._waypoints.length; i++) {
+  const waypoint = path._waypoints[i];
+  position.push(waypoint.x, waypoint.y, waypoint.z);
+}
+
+const lineGeo = new THREE.BufferGeometry();
+lineGeo.setAttribute('position', new THREE.Float32BufferAttribute(position, 3));
+
+const lineMat = new THREE.LineBasicMaterial({ color: 0xffffff });
+const lines = new THREE.LineLoop(lineGeo, lineMat);
+scene.add(lines);
 
 const time = new YUKA.Time();
 function animate() {
