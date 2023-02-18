be0ff29d45334b0139f597ac01a7a771c6556677
Noah Kim <dongwoo_333@hanmail.net>
2023-02-19 07:02:53 +1100

    18. Create A Self-Driving Car Using Three.js & Yuka - 3D/AI Tutorial

--- 18-yuka-self-driving-car/src/ts/scripts.ts
+++ 18-yuka-self-driving-car/src/ts/scripts.ts
@@ -1,9 +1,11 @@
 import * as THREE from 'three';
 import * as YUKA from 'yuka';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const renderer = new THREE.WebGLRenderer();
 
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x777777);
 
 document.body.appendChild(renderer.domElement);
 
@@ -11,20 +13,19 @@ const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-camera.position.set(0, 20, 0);
+camera.position.set(0, 10, 15);
 camera.lookAt(scene.position);
 
-// Vehicle Mesh
-const vehicleGeo = new THREE.ConeGeometry(0.1, 0.5, 8);
-vehicleGeo.rotateX(Math.PI * 0.5);
-const vehicleMat = new THREE.MeshNormalMaterial();
-const vehicleMesh = new THREE.Mesh(vehicleGeo, vehicleMat);
-vehicleMesh.matrixAutoUpdate = false;
-scene.add(vehicleMesh);
+// Add lights
+const ambientLight = new THREE.AmbientLight(0x333333);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(0, 10, 10);
+scene.add(directionalLight);
 
 // Vehicle Yuka
 const vehicle = new YUKA.Vehicle();
-vehicle.setRenderComponent(vehicleMesh, sync);
 
 function sync(entity: YUKA.GameEntity, renderComponent: any) {
   renderComponent.matrix.copy(entity.worldMatrix);
@@ -39,38 +40,49 @@ type Path = YUKA.Path & {
 // Path
 // @ts-ignore
 const path: Path = new YUKA.Path();
-path.add(new YUKA.Vector3(-4, 0, 4));
-path.add(new YUKA.Vector3(-6, 0, 0));
-path.add(new YUKA.Vector3(-4, 0, -4));
+path.add(new YUKA.Vector3(-6, 0, 4));
+path.add(new YUKA.Vector3(-11, 0, 0));
+path.add(new YUKA.Vector3(-6, 0, -11));
 path.add(new YUKA.Vector3(0, 0, 0));
-path.add(new YUKA.Vector3(4, 0, -4));
-path.add(new YUKA.Vector3(6, 0, 0));
+path.add(new YUKA.Vector3(8, 0, -8));
+path.add(new YUKA.Vector3(10, 0, 0));
 path.add(new YUKA.Vector3(4, 0, 4));
 path.add(new YUKA.Vector3(0, 0, 6));
 
 path.loop = true;
-
-vehicle.position.copy(path.current());
-
-const followPathBehavior = new YUKA.FollowPathBehavior(path, 0.5);
-vehicle.steering.add(followPathBehavior);
-
-// follow the path closely
-const onPathBehavior = new YUKA.OnPathBehavior(path);
-onPathBehavior.radius = 0.8;
-vehicle.steering.add(onPathBehavior);
-
-// vehicle.maxSpeed = 3;
-
-// Entity Manager
-const entityManager = new YUKA.EntityManager();
-entityManager.add(vehicle);
 
 const position = []; // (x, y, z)[]
 for (let i = 0; i < path._waypoints.length; i++) {
   const waypoint = path._waypoints[i];
   position.push(waypoint.x, waypoint.y, waypoint.z);
 }
+
+vehicle.position.copy(path.current());
+
+vehicle.maxSpeed = 3;
+
+const followPathBehavior = new YUKA.FollowPathBehavior(path, 3); // steering looks more natural
+vehicle.steering.add(followPathBehavior);
+
+// follow the path closely
+const onPathBehavior = new YUKA.OnPathBehavior(path);
+// onPathBehavior.radius = 0.8;
+vehicle.steering.add(onPathBehavior);
+
+// Entity Manager
+const entityManager = new YUKA.EntityManager();
+entityManager.add(vehicle);
+
+// Loader
+const loader = new GLTFLoader();
+loader.load('./assets/SUV.glb', function (glb) {
+  const model = glb.scene;
+  // model.scale.set(0.5, 0.5, 0.5);
+  scene.add(model);
+  model.matrixAutoUpdate = false;
+  vehicle.scale = new YUKA.Vector3(0.3, 0.3, 0.3);
+  vehicle.setRenderComponent(model, sync);
+});
 
 const lineGeo = new THREE.BufferGeometry();
 lineGeo.setAttribute('position', new THREE.Float32BufferAttribute(position, 3));
